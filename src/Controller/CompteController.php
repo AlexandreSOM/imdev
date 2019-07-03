964cc47ef9302a92ad5bca5cf0552cac823e5eb7
mail <som.alexandre@outlook.fr>
2019-07-03 09:50:43 +0200

    2nd commit

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -22,7 +22,7 @@ class CompteController extends AbstractController
 
         /**
      * @Route("/compte-ajout", name="compte-ajout", requirements={"compte-ajout"="^(?!register).+"})
-     * @Route("detail/{compte}/modifier", name="compte-modifier", requirements={"compte-modifier"="^(?!register).+"})
+     * @Route("detail-compte/{compte}/modifier", name="compte-modifier", requirements={"compte-modifier"="^(?!register).+"})
      */
     public function form(Request $request, Compte $compte = null)
     {
